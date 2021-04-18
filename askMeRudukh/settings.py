5f3ecedf9e01397ee79ca94be5c51346244824ee
Rudukh <Rudukh.Eugene@yandex.ru>
2021-04-18 22:34:30 +0300

    initial commit

--- askMeRudukh/settings.py
+++ askMeRudukh/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'askMeRudukh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
